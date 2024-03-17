d77adb912353171e5b3e602b582952143c6a0abb
Jhonata Carvalho <jhonata.carvalho@tascomtecnologia.com.br>
2024-03-17 18:34:45 -0300

    add: colors

---
+++ src/global/colors.ts
@@ -0,0 +1,11 @@
+export const lightColors = {
+  primary: "#3883D9",
+  secondary: "#FFF",
+  tertiary: "#636363",
+};
+
+export const darkColors = {
+  primary: "#3883D9",
+  secondary: "#1C1C1C",
+  tertiary: "#FFF",
+};
